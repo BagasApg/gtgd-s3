ede17b95fbed8b4337bdd95a02eb471b4d61d969
BagasApg <bagasapg@gmail.com>
2025-08-22 12:51:27 +0700

    minor changes

---
+++ Assets/Scripts/S3/GameManager Scripts/GameManager_TogglePause.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace S3
+{
+
+    public class GameManager_TogglePause : MonoBehaviour
+    {
+        private GameManager_Master master_Script;
+        private bool isPaused;
+        void OnEnable()
+        {
+
+        }
+
+        void OnDisable()
+        {
+
+        }
+
+        void setInitialReferences()
+        {
+            // master_Script = GetComponent
+        }
+
+        void togglePause()
+        {
+
+        }
+    }
+
+}
